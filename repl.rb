967928c74cb70df6d4e4cd19534c6dcdc5b35f29
Artoria <Artoria>
2013-01-11 02:40:05 +0800

    shellcommands

--- repl.rb
+++ repl.rb
@@ -1,33 +1,61 @@
 alias my_eval eval
+
+
+
+require 'irb'
+
+
+
+module IREPL
+  EVALS = []
+  def cmd(reg, &block)
+    EVALS.push [reg, block]
+  end
+
+  def set_prompt
+    $PS1 = "Dir.getwd.tr('/', '\\\\') + '>'"
+    cd = my_eval $PS1
+
+    def cd.dup
+       my_eval $PS1
+    end
+
+    IRB.conf[:PROMPT][:CUSTOM] = {
+       :PROMPT_I => cd,
+       :PROMPT_S => cd,
+       :PROMPT_C => cd,
+       :PROMPT_N => cd,
+       :RETURN => "=> %s\n"
+    }
+    IRB.conf[:PROMPT_MODE] = :CUSTOM
+  end
+
+  def eval(str, *a)
+      r = EVALS + [[/.*/, method(:my_eval)]]
+      x = r.select{|k| k[0] =~ str}
+      ret = false
+      x.each{|y|
+        y[0] =~ str
+        $ARG = $~
+        ret = y[1].call(str, *a)
+        break if ret
+      }
+      set_prompt unless $PS1
+      ret
+  end
+
+end
+
+
+
+class Object
+  include IREPL
+end
+
 $: << File.dirname(__FILE__)
 begin
   load ".irbshrc" 
 rescue LoadError
-end
-require 'irb'
-
-$PS1 = "Dir.getwd.tr('/', '\\\\') + '>'"
-$PS_ = 0
-def eval(str, *a)
-  ret = if str[0] == '!'
-    system str[1, str.length-1]
-  else
-    my_eval str, *a
-  end
-  cd = my_eval $PS1
-  def cd.dup
-    my_eval $PS1
-  end
-  $PS_ += 1
-  IRB.conf[:PROMPT][:CUSTOM] = {
-   :PROMPT_I => cd,
-   :PROMPT_S => cd,
-   :PROMPT_C => cd,
-   :PROMPT_N => cd,
-   :RETURN => "=> %s\n"
-  }
-  IRB.conf[:PROMPT_MODE] = :CUSTOM
-  ret
 end
 
 IRB.start
