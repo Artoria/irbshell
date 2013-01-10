fbedb3a09ba8b4d542cc7b4cd934c826f99a9d98
Artoria <Artoria>
2013-01-11 05:43:04 +0800

    Mingw-get

--- ar-get.rb
+++ ar-get.rb
@@ -1,7 +1,8 @@
 #!irbshell
 
-!wget "https://github.com/Artoria/irbshell/blob/master/ar-get.list"
+!wget "https://github.com/Artoria/irbshell/raw/master/ar-get.list"
 x = File.read("ar-get.list").split(/\n/)
 if ARGV[0] == 'get'
-  eval("!wget --no-check-certificate \"https://github.com/Artoria/irbshell/blob/master/ar-get/#{ARGV[1]}/get.rb\"")
+  eval("!wget --no-check-certificate \"https://github.com/Artoria/irbshell/raw/master/ar-get/#{ARGV[1]}/get.rb\"")
+  get.rb
 end
